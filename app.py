f04c7f9144ce6d262320a109256f11a1b787a9bd
Hemanth <karumanchihemanth08@gmail.com>
2026-06-13 23:15:50 +0530

    Initial project setup & architecture

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="AI Resume & Interview Copilot",
+    page_icon="🤖"
+)
+
+st.title("AI Resume & Interview Copilot")
+
+st.write("Upload your resume and compare it with a job description to get AI-Powered insights")
